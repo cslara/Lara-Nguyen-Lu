09a373c18bd06118c6369cfa29fc2e590950d4f0
Cristina <cslara@uh.edu>
2019-10-22 21:06:07 -0500

    Cristina created Disease table

---
+++ db/migrate/20191023020511_create_diseases.rb
@@ -0,0 +1,10 @@
+class CreateDiseases < ActiveRecord::Migration[6.0]
+  def change
+    create_table :diseases do |t|
+      t.string :name
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
