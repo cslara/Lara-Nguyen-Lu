3d1d3c999142e36997c66a318735bf9f6dd4c868
Wei Lu <lennonbellaire@gmail.com>
2019-10-22 21:07:45 -0500

    Wei Animal

---
+++ app/views/animals/_animal.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! animal, :id, :animal_name, :animal_type, :created_at, :updated_at
+json.url animal_url(animal, format: :json)
---
+++ db/migrate/20191023020651_create_animals.rb
@@ -0,0 +1,10 @@
+class CreateAnimals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :animals do |t|
+      t.string :animal_name
+      t.string :animal_type
+
+      t.timestamps
+    end
+  end
+end
